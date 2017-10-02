5832bb216567aae313e1764174c539993994be39
sgawne2 <sgawne2@gmail.com>
2017-10-01 21:24:31 -0700

    functional components

--- src/components/app.js
+++ src/components/app.js
@@ -1,2 +1,31 @@
+import React from 'react';
 
+import Table from './table'
 
+const students = [
+	{
+		student: 'Sean',
+		course: 'Creativity',
+		grade: 1
+	},
+	{
+		student: 'who cares',
+		course: 'React',
+		grade: 100
+	},
+	{
+		student: 'some guy',
+		course: 'React',
+		grade: 100
+	}
+];
+
+const App = (props) => (
+	//<h1>My first React functional component</h1>
+	<div className="container">
+		<h1>Student Grade Table</h1>
+		<Table data={ students } />
+	</div>
+);
+
+export default App;
